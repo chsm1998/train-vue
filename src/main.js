8f2f75a9c54a4bfa7461dbf2af4ab304efbeed49
chsm1998 <1978364643@qq.com>
2018-09-27 14:55:15 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from "vue";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from "./App.vue";
 import router from "./router";
+import axios from 'axios'
+
+Vue.use(ElementUI);
+
+axios.defaults.baseURL = "http://localhost"
+axios.defaults.withCredentials = true
+
+Vue.prototype.axios = axios
+Vue.prototype.baseUrl = 'http://localhost'
+const TITLE = "第二商城-"
 
 Vue.config.productionTip = false;
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.title) {
+        document.title = TITLE + to.meta.title
+    }
+    next()
+})
 
 new Vue({
   router,
